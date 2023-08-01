462356a4e03f0a998155272ffffbdfdecbae165b
milidv <131211714+milidv@users.noreply.github.com>
2023-08-01 16:51:41 -0300

    se modifica script para segunda preentrega

--- js/script.js
+++ js/script.js
@@ -1,40 +1,59 @@
-function dividirGasto() {
-  // Pedir los datos al usuario
-  //==================Variables tipo string=======================
-  let nombre = prompt("Ingrese su nombre:");
-  let tipoGasto = prompt("Ingrese el tipo de gasto (super, comida, transporte):");
-  let incluirPropina = prompt("¿Desea incluir propina? (Sí/No)");
+    //==================Objeto: info gastos=======================
+const gasto = {
+    nombre: "",
+    tipoGasto: "",
+    incluirPropina: "",
+    montoGasto: 0,
+    cantidadPersonas: 0,
+    montoPropina: 0,
   
-  //==================Variables tipo number=======================
-  let montoGasto = 0
-  let cantidadPersonas = 0
-  let gastoTotal = 0
-  let montoPropina = 0
-  let pagoPorPersona = 0
-
-  do {
-      montoGasto = parseFloat(prompt("Ingrese un número en el monto del gasto:"))
-  } while (isNaN(montoGasto) || montoGasto <= 0)
-
-  do {
-      cantidadPersonas = parseInt(prompt("Ingrese la cantidad de personas:"))
-  } while (isNaN(cantidadPersonas) || cantidadPersonas < 1)
-
-// Calcular el monto de la propina si es necesario
-incluirPropina = incluirPropina.toLowerCase()
-if (incluirPropina == "si"){
-    // Calcular el 10% del monto del gasto
-    montoPropina = montoGasto * 0.1;
-}
-
-  // Calcular el gasto total incluyendo la propina
-  gastoTotal = montoGasto + montoPropina;
-
-  // Calcular el dinero que debe pagar cada persona
-  pagoPorPersona = gastoTotal / cantidadPersonas;
-
-  // Mostrar el resultado en la consola
-  console.log(nombre + ", cada persona debe pagar $" + pagoPorPersona + " por " + tipoGasto);
-}
-
-dividirGasto()
+    // Pedir los datos al usuario (método)
+    pedirDatos: function() {
+      this.nombre = prompt("Ingrese su nombre:");
+      this.tipoGasto = prompt("Ingrese el tipo de gasto (super, comida, transporte):");
+      this.incluirPropina = prompt("¿Desea incluir propina? (Sí/No)").toLowerCase();
+  
+      do {
+        this.montoGasto = parseFloat(prompt("Ingrese el monto del gasto:"));
+      } while (isNaN(this.montoGasto) || this.montoGasto <= 0);
+  
+      do {
+        this.cantidadPersonas = parseInt(prompt("Ingrese la cantidad de personas:"));
+      } while (isNaN(this.cantidadPersonas) || this.cantidadPersonas < 1);
+    },
+  
+    // Calcular el monto de la propina si es necesario (método)
+    calcularPropina: function() {
+      if (this.incluirPropina === "si") {
+        // Calcular el 10% del monto del gasto
+        this.montoPropina = this.montoGasto * 0.1;
+      }
+    },
+  
+    // Método para calcular el gasto total incluyendo la propina
+    calcularGastoTotal: function() {
+      return this.montoGasto + this.montoPropina;
+    },
+  
+    // Método para calcular el dinero que debe pagar cada persona
+    calcularPagoPorPersona: function() {
+      const gastoTotal = this.calcularGastoTotal();
+      return (gastoTotal / this.cantidadPersonas).toFixed(2);
+    },
+  
+    // Método para mostrar el resultado en la consola
+    mostrarResultado: function() {
+      const pagoPorPersona = this.calcularPagoPorPersona();
+      console.log(this.nombre + ", cada persona debe pagar $" + pagoPorPersona + " por " + this.tipoGasto);
+    }
+  };
+  
+  // Función de orden superior para ejecutar las funciones en orden
+  function calcularYMostrarGasto() {
+    gasto.pedirDatos();
+    gasto.calcularPropina();
+    gasto.mostrarResultado();
+  }
+  
+  // Ejecutar la función principal
+  calcularYMostrarGasto();
