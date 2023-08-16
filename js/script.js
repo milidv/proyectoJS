9243994dd72481a3b13c399f14a356617884f380
milidv <131211714+milidv@users.noreply.github.com>
2023-08-15 21:25:47 -0300

    se agrego estilo css, elementos html y json

--- js/script.js
+++ js/script.js
@@ -7,22 +7,27 @@
         cantidadPersonas: 0,
         montoPropina: 0,
       
-        // Pedir los datos al usuario (método)
+            //==================Método:Pedir los datos al usuario=======================
+
         pedirDatos: function() {
-          this.nombre = prompt("Ingrese su nombre:");
-          this.tipoGasto = prompt("Ingrese el tipo de gasto (super, comida, transporte):");
-          this.incluirPropina = prompt("¿Desea incluir propina? (Sí/No)").toLowerCase();
-      
-          do {
-            this.montoGasto = parseFloat(prompt("Ingrese el monto del gasto:"));
-          } while (isNaN(this.montoGasto) || this.montoGasto <= 0);
-      
-          do {
-            this.cantidadPersonas = parseInt(prompt("Ingrese la cantidad de personas:"));
-          } while (isNaN(this.cantidadPersonas) || this.cantidadPersonas < 1);
+          this.nombre = document.getElementById("nombre").value;
+          this.tipoGasto = document.getElementById("tipoGasto").value;
+          this.incluirPropina = document.getElementById("incluirPropina").value;
+          this.montoGasto = parseFloat(document.getElementById("montoGasto").value);
+          this.cantidadPersonas = parseInt(document.getElementById("cantidadPersonas").value);
         },
+    
+        //   do {
+        //     this.montoGasto = parseFloat(prompt("Ingrese el monto del gasto:"));
+        //   } while (isNaN(this.montoGasto) || this.montoGasto <= 0);
       
-        // Calcular el monto de la propina si es necesario (método)
+        //   do {
+        //     this.cantidadPersonas = parseInt(prompt("Ingrese la cantidad de personas:"));
+        //   } while (isNaN(this.cantidadPersonas) || this.cantidadPersonas < 1);
+        // },
+      
+                    //==================Método:Calcular el monto de la propina si es necesario=======================
+
         calcularPropina: function() {
           if (this.incluirPropina === "si") {
             // Calcular el 10% del monto del gasto
@@ -30,30 +35,72 @@
           }
         },
       
-        // Calcular el gasto total incluyendo la propina
+       //================== Calcular el gasto total incluyendo la propina==================
         calcularGastoTotal: function() {
           return this.montoGasto + this.montoPropina;
         },
       
-        // Método para calcular el dinero que debe pagar cada persona
+        //================== Método: Calcular el dinero que debe pagar cada persona==================
         calcularPagoPorPersona: function() {
           const gastoTotal = this.calcularGastoTotal();
           return (gastoTotal / this.cantidadPersonas).toFixed(2);
         },
       
-        // Método para mostrar el resultado en la consola
+        //================== Método: mostrar el resultado en la consola ==================
         mostrarResultado: function() {
           const pagoPorPersona = this.calcularPagoPorPersona();
           console.log(this.nombre + ", cada persona debe pagar $" + pagoPorPersona + " por " + this.tipoGasto);
         }
       };
       
-      // Función de orden superior para ejecutar las funciones en orden
+      //================== Función de orden superior:ejecutar las funciones en orden==================
       function calcularYMostrarGasto() {
         gasto.pedirDatos();
         gasto.calcularPropina();
         gasto.mostrarResultado();
       }
       
-      // Ejecutar la función principal
-      calcularYMostrarGasto();
+
+//================== Guardar los datos del gasto en JSON ==================
+function guardarDatos(gasto) {
+  const datos = JSON.stringify(gasto);
+  localStorage.setItem("gasto", datos);
+}
+
+//================== Leer los datos del gasto de JSON ==================
+function leerDatos() {
+  const datos = localStorage.getItem("gasto");
+  if (datos) {
+    return JSON.parse(datos);
+  } else {
+    return null;
+  }
+}
+
+// Mostrar el resultado en la consola
+// function mostrarResultado(gasto) {
+//   const pagoPorPersona = gasto.calcularPagoPorPersona();
+//   console.log(gasto.nombre + ", cada persona debe pagar $" + pagoPorPersona + " por " + gasto.tipoGasto);
+
+
+// Mostrar el resultado en HTML
+function mostrarResultado() {
+const pagoPorPersona = this.calcularPagoPorPersona();
+const h3Resultado = document.createElement("h3");
+h3Resultado.textContent = "${this.nombre}, cada persona debe pagar $${pagoPorPersona} por ${this.tipoGasto}";
+document.resultado.appendChild(h3Resultado);
+}
+// ==================Ejecutar la función principal==================
+calcularYMostrarGasto();
+
+    //==================DOM: botón calcular =======================
+
+
+const botonCalcular = document.getElementById("calcular");
+botonCalcular.style.backgroundColor = "#a4ac86";
+botonCalcular.style.color = "white";
+botonCalcular.style.fontSize = "16px";
+botonCalcular.style.fontWeight = "bold";
+botonCalcular.style.borderRadius = "5px";
+botonCalcular.style.cursor = "pointer";
+
